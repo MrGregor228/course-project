68802b8a5f528d3f84d5989b4804be798cab86d9
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2021-05-05 14:32:46 +0300

    New update
    - A few sections added
    - Limited sale cards added
    - Moment JS timers for cards
    - Footer added

--- js/main-page.js
+++ js/main-page.js
@@ -1,6 +1,33 @@
+// Timer for sales
+function setSaleInterval(eventDate, cardID) {
+    let eventTime = new Date(eventDate).getTime();
+    let currentTime = new Date().getTime(); 
+    let diffTime = eventTime - currentTime;
+    let duration = moment.duration(diffTime);
+    let interval = 1000;
+    function addZeroToNumber(num) {
+        if(num <= 9) {
+            return '0' + num;
+        } else return num;
+    }
+    setInterval(function () {
+        duration = moment.duration(duration - interval, 'milliseconds');
+        document.querySelector(`[data-good-id="${cardID}"] .sale-container`).textContent = `${addZeroToNumber(duration.days())}д : ${addZeroToNumber(duration.hours())}ч : ${addZeroToNumber(duration.minutes())}м : ${addZeroToNumber(duration.seconds())}с`;
+    }, interval);
+}
+
 window.addEventListener('DOMContentLoaded', () => {
     let toggle_button = document.querySelector('.toggle-button'),
         menu = document.getElementById('menu');
+
+    let saleCards = document.querySelectorAll('.card.sale');
+    if (saleCards.length == 1) {
+        setSaleInterval(saleCards[0].dataset.expiryDate, saleCards[0].dataset.goodId);
+    } else if (saleCards.length > 1) {
+        saleCards.forEach(card => {
+            setSaleInterval(card.dataset.expiryDate, card.dataset.goodId);
+        });
+    }
 
     toggle_button.addEventListener('click', () => {
         menu.classList.toggle('show');
@@ -17,10 +44,12 @@ window.addEventListener('DOMContentLoaded', () => {
             menu.classList.remove('show');
         }
     });
+
+
 });
 
 // Swiper Slider config
-(function name() {
+(function () {
     const mySwiper = new Swiper('.swiper-container', {
         // Optional parameters
         direction: 'horizontal',
