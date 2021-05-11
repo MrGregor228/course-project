c2a9ab2d06617a2293ebaa01b65644a108e3f53c
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2021-05-11 13:19:29 +0300

    Половина реализации добавления товаров в избранное

--- js/toggleModals.js
+++ js/toggleModals.js
@@ -1,8 +1,41 @@
 const button_LikeModal = document.getElementById('toggle-like-modal');
+let like_buttons = document.querySelectorAll('div.like');
+const liked_cards = [];
+
 button_LikeModal.addEventListener('click', () => {
     if (document.querySelector('.modal-likes').style.display == "none") {
         document.querySelector('.modal-likes').style.display = "flex";
     } else {
         document.querySelector('.modal-likes').style.display == "none";
     }
+});
+
+like_buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        let target = e.target;
+        let closestDatasetCard = target.closest('.card').dataset.goodId;
+        if (!target.closest('.card').dataset.liked) {
+            target.closest('.card').dataset.liked = "true";
+            button.querySelector('img').src = 'icons/heart-selected.svg';
+            let card = {
+                dataID: closestDatasetCard,
+                liked: target.closest('.card').dataset.liked
+            };
+            liked_cards.push(card);
+
+            
+        } else {
+            let card = {
+                dataID: closestDatasetCard,
+                liked: target.closest('.card').dataset.liked
+            };
+            liked_cards.filter((card,i) => {
+                if (card.dataID == closestDatasetCard) {
+                    liked_cards.splice(i,1);
+                }
+            });
+            delete target.closest('.card').dataset.liked;
+            button.querySelector('img').src = 'icons/card-heart.svg';
+        }
+    });
 });
